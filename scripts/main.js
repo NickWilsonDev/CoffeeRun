c41a303e56ad1e3b63eb67d08cd0d49ac16a3037
Nick Wilson <logikfx@gmail.com>
2018-03-01 15:40:56 -0500

    added populate button that posts a few orders to the server, added feature where selected item to delete waits 2 seconds until removing from local list

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,14 @@
 var orderList;
 
 
+var deleteRequest = function (element) {
+        //var index = findByAttr(orderList, 'email', this.id);
+        //orderList.splice(index, 1);
+        console.log("deleting");
+        $(element).remove();
+}
+
+
 var makeOrderListItem = function (order) {
     console.log("-----------------");
     console.log(order);
@@ -15,14 +23,22 @@ var makeOrderListItem = function (order) {
                     + order.strength);
     // listener for removing
     $listElement.click( function(event) {
-    $.ajax({
-        url: "http://dc-coffeerun.herokuapp.com/api/coffeeorders/" 
-                                            + order.emailAddress,
-        type: 'DELETE',
-    });
+    
         var index = findByAttr(orderList, 'email', this.id);
         orderList.splice(index, 1);
-        $(this).remove();
+        //$(this).remove();
+        $listElement.css("background-color","green");
+        $listElement.off('click');
+        window.setTimeout(function() {
+                    console.log("deleting");
+                    $($listElement).remove();
+                    }, 2000);
+        $.ajax({
+            url: "http://dc-coffeerun.herokuapp.com/api/coffeeorders/" 
+                                            + order.emailAddress,
+            type: 'DELETE',
+        });
+
     });
 
     $('#previousOrders').append($listElement);
@@ -85,5 +101,36 @@ $form[0].addEventListener('submit', function(event) {
       console.log(resp)
     });
     $form[0].reset();
+});
+
+
+
+
+
+orders = [
+    {"strength":0,"flavor":"None","size":"tall","emailAddress":"person@email.com","coffee":"Coffee"}, 
+    {"strength":3,"flavor":"mocha","size":"tall","emailAddress":"jamlicious@gmail.com","coffee":"latte"},
+    {"flavor":"almond","strength":30,"size":"short","emailAddress":"xyz@who.com","coffee":"latte"},
+    {"strength":10,"flavor":"caramel","size":"tall","emailAddress":"uni@gallop.org","coffee":"love"},
+]
+
+
+var $popBtn = $('#popBtn');
+$popBtn[0].addEventListener('click', function(event) {
+    event.preventDefault();
+    for (var i = 0; i < orders.length; i++) {
+        var order = {
+            coffee: orders[i]['coffee'],
+            emailAddress: orders[i]['emailAddress'],
+            size: orders[i]['size'],
+            flavor: orders[i]['flavor'],
+            strength: orders[i]['strength']
+        };
+
+        console.log(order);
+        $.post("http://dc-coffeerun.herokuapp.com/api/coffeeorders", order, function(resp) {
+            console.log(resp)
+        });
+    }
 });
 
